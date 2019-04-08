2abe8947df1e7c5aac699ed74e41118b0b1d12ab
yahor_bazyk <yegor.bazyk@gmail.com>
2019-04-08 16:59:26 +0300

    added ex-2

---
+++ ex-2.js
@@ -0,0 +1,10 @@
+function getCounter() {
+    let counter = 0;
+    return function() {
+      return counter++;
+    }
+  }
+  let count = getCounter();
+  console.log(count());  // 0
+  console.log(count());  // 1
+  console.log(count());  // 2
