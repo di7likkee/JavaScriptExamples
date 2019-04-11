ba48bb0bfc9d5fab28200e3b5b416671174d5520
yahor_bazyk <yegor.bazyk@gmail.com>
2019-04-11 16:47:10 +0300

    fixed

--- ex-4.js
+++ ex-4.js
@@ -16,3 +16,20 @@ var willIGetNewPhone = new Promise(
 
     }
 );
+
+// Вызываем промис
+var askMom = function () {
+    willIGetNewPhone
+        .then(function (fulfilled) {
+            // yay, you got a new phone
+            console.log(fulfilled);
+         // output: { brand: 'Samsung', color: 'black' }
+        })
+        .catch(function (error) {
+            // oops, mom don't buy it
+            console.log(error.message);
+         // output: 'mom is not happy'
+        });
+};
+
+askMom();
