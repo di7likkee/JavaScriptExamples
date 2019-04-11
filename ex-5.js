b7191726feadfcfc8042bb994fa12189a3c85f13
yahor_bazyk <yegor.bazyk@gmail.com>
2019-04-11 16:44:05 +0300

    ex-5

---
+++ ex-5.js
@@ -0,0 +1,16 @@
+// Вызываем промис
+var askMom = function () {
+    willIGetNewPhone
+        .then(function (fulfilled) {
+            // yay, you got a new phone
+            console.log(fulfilled);
+         // output: { brand: 'Samsung', color: 'black' }
+        })
+        .catch(function (error) {
+            // oops, mom don't buy it
+            console.log(error.message);
+         // output: 'mom is not happy'
+        });
+};
+
+askMom();
